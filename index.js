0be94eb2987a234153c037c92431908c2c468fe2
passionate-array-0100 <robertkahn92@gmail.com>
2020-06-17 19:52:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name){
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+function happyCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
